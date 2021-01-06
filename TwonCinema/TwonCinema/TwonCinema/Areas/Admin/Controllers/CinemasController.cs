c61ab7e54a461c28db848e11d0c47ad3e16d87ac
Le Minh Nhat <leminhnhat1133@gmail.com>
2021-01-06 22:43:10 +0700

    Chinh sua Cinema thanh 1 trang

---
+++ TwonCinema/TwonCinema/TwonCinema/Areas/Admin/Controllers/CinemasController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TwonCinema.Areas.Admin.Data;
+using TwonCinema.Areas.Admin.Models;
+
+namespace TwonCinema.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CinemasController : Controller
+    {
+        private readonly DPContext _context;
+
+        public CinemasController(DPContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Cinemas
+        public async Task<IActionResult> Index(int status=1)
+        {
+            var dPContext = _context.Cinemas.Include(c => c.Staf).Where(c=>c.Status==status);
+            ViewBag.ListCinema =await dPContext.ToListAsync();
+            ViewData["Manager_ID"] = new SelectList(_context.Stafs, "ID", "Email");
+            return View();
+        }
+
+       
+   
+
+        // POST: Admin/Cinemas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Name,Address,Hotline,Status,Manager_ID")] Cinema cinema)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cinema);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+      
+        // POST: Admin/Cinemas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit([Bind("ID,Name,Address,Hotline,Status,Manager_ID")] Cinema cinema)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cinema);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CinemaExists(cinema.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        private bool CinemaExists(int id)
+        {
+            return _context.Cinemas.Any(e => e.ID == id);
+        }
+    }
+}
